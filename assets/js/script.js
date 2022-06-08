c8994d99e547455dade41a764124ee4f7f033582
Andrea Paez <andrea.paez86@gmail.com>
2022-06-08 19:16:45 -0400

    better

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,30 @@
 const price = 999;
+let b = document.querySelector("#button");
 
 fixedPrice = document.querySelector("#price");
 fixedPrice.innerHTML = price;
 
-function calculate() {
-  quantity = document.querySelector("#quantity");
-  showQuantity = document.querySelector("#show-quantity");
-  showQuantity.innerHTML = quantity.value;
-  total = Number(quantity.value) * Number(price);
-  totalPrice = document.querySelector("#total-price");
-  totalPrice.innerHTML = total;
+
+quantity = document.querySelector("#quantity");
+showQuantity = document.querySelector("#show-quantity");
+showQuantity.innerHTML = quantity.value;
+
+b.addEventListener("click", function() {
+total = Number(quantity.value) * Number(price);
+totalPrice = document.querySelector("#total-price");
+totalPrice.innerHTML = total;
+  
+});
 
 
   
-    function changeBackground() {
-        let color = document.querySelector("#color").value;
-        document.querySelector("#show-color").style.backgroundColor = color;
-    }
-    changeBackground();
+function changeBackground() {
+  let color = document.querySelector("#color").value;
+  document.querySelector("#show-color").style.backgroundColor = color;
+  }
+changeBackground();
 
-}
+
 
 
 
