2c42a9aded2ab78dcbb0529a335b69710d232a5d
Andrea Paez <andrea.paez86@gmail.com>
2022-06-08 19:20:23 -0400

    last last

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ let b = document.querySelector("#button");
 fixedPrice = document.querySelector("#price");
 fixedPrice.innerHTML = price;
 
-
-quantity = document.querySelector("#quantity");
-showQuantity = document.querySelector("#show-quantity");
-showQuantity.innerHTML = quantity.value;
-
 b.addEventListener("click", function() {
 total = Number(quantity.value) * Number(price);
 totalPrice = document.querySelector("#total-price");
 totalPrice.innerHTML = total;
+quantity = document.querySelector("#quantity");
+showQuantity = document.querySelector("#show-quantity");
+showQuantity.innerHTML = quantity.value;
+
+
   
 });
 
